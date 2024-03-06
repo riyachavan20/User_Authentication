7b000f6b4c6d8bd28552f391c1e3e498bacd28c1
riya chavan <riyachavan71939@gmail.com>
2024-03-06 15:38:57 +0530

    CHANGES

--- server/app.js
+++ server/app.js
@@ -3,16 +3,27 @@ const keysecret = "arqfgstrafrwthkjiugthvfdckigtcft"
 const express = require("express");
 const session = require("express-session");
 const app = express();
-const BASE_URL=process.env.BASE_URL
+const FRONTEND_URL=process.env.FRONTEND_URL
 require("../db/conn");
 const router = require("./routes/router");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const port = 8000;
-
+const path = require("path");  
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
 
 // Add session middleware
 app.use(
